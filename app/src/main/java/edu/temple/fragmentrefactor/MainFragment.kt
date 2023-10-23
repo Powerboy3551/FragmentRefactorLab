80e974f5b4bfa3fed1daaf7ece12467a95bd95c0
Powerboy3551 <145390119+Powerboy3551@users.noreply.github.com>
2023-10-23 15:42:51 -0400

    Refractored the program in order to use fragment

---
+++ app/src/main/java/edu/temple/fragmentrefactor/MainFragment.kt
@@ -0,0 +1,37 @@
+package edu.temple.fragmentrefactor
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val changeButton = view.findViewById<Button>(R.id.changeButton)
+        val nameEditText = view.findViewById<EditText>(R.id.nameEditText)
+        val displayTextView = view.findViewById<TextView>(R.id.displayTextView)
+
+        changeButton.setOnClickListener {
+            val name = nameEditText.text.toString()
+
+            displayTextView.text = if (name.isNotBlank()) {
+                "Hello, $name!"
+            } else {
+                "Please enter your name"
+            }
+        }
+    }
+}
